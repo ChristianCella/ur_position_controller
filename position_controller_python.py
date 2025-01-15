876e5464ec55f31300e863f037bed539dba45716
ChristianCella <christian.cella@polimi.it>
2025-01-15 17:28:40 +0100

    Minor modifications to both controllers

--- position_controller_python.py
+++ position_controller_python.py
@@ -5,6 +5,7 @@ from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Float64MultiArray
 import numpy as np
+import time
 import tf.transformations as tf
 from scipy.spatial.transform import Rotation as R
 
@@ -32,65 +33,22 @@ def compute_orientation_error(tcp_orientation, desired_orientation):
     ])
     return orientation_error
 
-class LinearTimeLaw:
-    ''' 
-    Linear interpolation between two points in a given time duration.
-    This outputs the path function p(s(t)), with s(t) being the time law.
-    The formula is the following:
-        p(s(t)) = x_i + (x_f - x_i) * s(t)
-        - x_i: initial point
-        - x_f: final point
-        - t: specific time
-        - dt: time span given by the publishing node (1 / 50)
-        - t/dt = s(t) = alpha (time parametrization, s belongs to [0, 1])   
-    '''
-    def __init__(self, start, end, duration):
-        self.start = start
-        self.end = end
-        self.duration = duration
-
-    def evaluate(self, t):
-        if t > self.duration:
-            return self.end
-        
-        # This is the linear interpolation formula on the abscissa: t will take values multiples of 1/500=0.002
-        alpha = t / self.duration
-
-        # What you are returning now is p(s(t)) = x_i + (x_f - x_i) * s(t)
-        return self.start + alpha * (self.end - self.start)
-
-class Interpolator: 
-    ''' 
-    Interpolator class to generate a trajectory between two points in a given time duration.
-    The path function is a function of time s(t) that returns 
-        the desired point at time t (you can define a lambda function that works with 'LinearTimeLaw').
-    '''
-    def __init__(self, path_function, duration):
-        self.path_function = path_function
-        self.duration = duration
-
-    def discretize_trajectory(self, rate, start_time):
-        sampling_time = 1.0 / rate # 0.002 in case of 500 Hz
-        time = start_time # Usually 0 at the beginning
-        points = []
-
-        # Until you reach the time taken by the publishing node to send the desired pose
-        while time < self.duration:
-            points.append(self.path_function(time))
-            time += sampling_time
-
-        remaining_time = max(0.0, self.duration - time)
-        return points, remaining_time
-
 class Kinematics:
+
+    ''' Class constructor '''
     def __init__(self, robot_description):
         success, tree = treeFromUrdfModel(URDF.from_xml_string(robot_description))
         if not success:
             rospy.logerr("Failed to construct KDL tree")
             return
+        
+        # Initialize the KDL kinematics tree
         self.tree = tree
 
     def set_chain(self, start, end):
+        ''' 
+        Define the kinematic chain from the start link to the end link.
+        '''
         self.chain = self.tree.getChain(start, end)
         if not self.chain:
             rospy.logerr(f"Failed to create KDL chain from {start} to {end}.")
@@ -100,17 +58,33 @@ class Kinematics:
         self.jac_solver = kdl.ChainJntToJacSolver(self.chain)
 
     def compute_fk(self, joints):
+        ''' 
+        Compute the forward kinematics given the joints positions.
+        The joints positions are updated with the joint_state_callback method.
+        '''
         joint_array = kdl.JntArray(len(joints))
         for i, joint in enumerate(joints):
             joint_array[i] = joint
+
+        # Compute the cartesian psoe starting from the joints positions
         cartesian_pose = kdl.Frame()
         result = self.fk_solver.JntToCart(joint_array, cartesian_pose)
         if result < 0:
             rospy.logerr("Failed to calculate forward kinematics")
             return None
+
         return cartesian_pose
 
     def compute_jacobian(self, joints):
+        ''' 
+        Compute the Jacobian matrix given the joints positions.
+        - x_dot = J(q) * q_dot ==> q_dot = J^+ * x_dot
+        - x_dot = [v_(1x3), w_(1x3)]^T
+        In this mthod, we only compute:
+        - J(q) = [J_v(q), J_w(q)], with 
+            - J_(v,i) = z_(i-1) x (p_n - p_(i-1))
+            - J_(w,i) = z_(i-1)
+        '''
         joint_array = kdl.JntArray(len(joints))
         for i, joint in enumerate(joints):
             joint_array[i] = joint
@@ -124,14 +98,124 @@ class Kinematics:
         for i in range(jacobian.rows()):
             for j in range(jacobian.columns()):
                 jacobian_np[i, j] = jacobian[i, j]
+
         return jacobian_np
 
+class TimeLaw:
+    """
+    Base class for all time laws. It defines the interface for evaluate,
+    evaluate_derivative, and get_parameters methods.
+    """
+    def evaluate(self, t):
+        raise NotImplementedError("Evaluate method must be implemented in subclasses.")
+
+    def evaluate_derivative(self, t):
+        raise NotImplementedError("Evaluate derivative method must be implemented in subclasses.")
+
+    def get_parameters(self):
+        raise NotImplementedError("Get parameters method must be implemented in subclasses.")
+
+class LinearTimeLaw(TimeLaw):
+    """
+    Implements linear interpolation between two points.
+    """
+    def __init__(self, start, end, duration):
+        self.start = start
+        self.end = end
+        self.duration = duration
+
+    def evaluate(self, t):
+        if t > self.duration:
+            return self.end
+        alpha = t / self.duration
+        return self.start + alpha * (self.end - self.start)
+
+    def evaluate_derivative(self, t):
+        return (self.end - self.start) / self.duration
+
+    def get_parameters(self):
+        return [self.start, self.end]
+
+class CubicalTimeLaw(TimeLaw):
+    """
+    Implements cubic interpolation between two points with specified velocities.
+    """
+    def __init__(self, start, end, start_velocity, end_velocity, duration):
+        self.a0 = start
+        self.a1 = start_velocity
+        self.a2 = (-3 * (start - end) - (2 * start_velocity + end_velocity) * duration) / duration**2
+        self.a3 = (2 * (start - end) + (start_velocity + end_velocity) * duration) / duration**3
+        self.duration = duration
+
+    def evaluate(self, t):
+        if t > self.duration:
+            return self.a0 + self.a1 * self.duration + self.a2 * self.duration**2 + self.a3 * self.duration**3
+        return self.a0 + self.a1 * t + self.a2 * t**2 + self.a3 * t**3
+
+    def evaluate_derivative(self, t):
+        return self.a1 + 2 * self.a2 * t + 3 * self.a3 * t**2
+
+    def get_parameters(self):
+        return [self.a0, self.a1, self.a2, self.a3]
+
+class QuinticTimeLaw(TimeLaw):
+    """
+    Implements quintic interpolation between two points with specified velocities and accelerations.
+    """
+    def __init__(self, start, end, start_velocity, end_velocity, start_acceleration, end_acceleration, duration):
+        self.a0 = start
+        self.a1 = start_velocity
+        self.a2 = 0.5 * start_acceleration
+        self.a3 = (20 * (end - start) - (8 * end_velocity + 12 * start_velocity) * duration -
+                   (3 * end_acceleration - start_acceleration) * duration**2) / (2 * duration**3)
+        self.a4 = (30 * (start - end) + (14 * end_velocity + 16 * start_velocity) * duration +
+                   (3 * end_acceleration - 2 * start_acceleration) * duration**2) / (2 * duration**4)
+        self.a5 = (12 * (end - start) - 6 * (end_velocity + start_velocity) * duration -
+                   (end_acceleration - start_acceleration) * duration**2) / (2 * duration**5)
+        self.duration = duration
+
+    def evaluate(self, t):
+        if t > self.duration:
+            return (self.a0 + self.a1 * self.duration + self.a2 * self.duration**2 +
+                    self.a3 * self.duration**3 + self.a4 * self.duration**4 + self.a5 * self.duration**5)
+        return self.a0 + self.a1 * t + self.a2 * t**2 + self.a3 * t**3 + self.a4 * t**4 + self.a5 * t**5
+
+    def evaluate_derivative(self, t):
+        return self.a1 + 2 * self.a2 * t + 3 * self.a3 * t**2 + 4 * self.a4 * t**3 + 5 * self.a5 * t**4
+
+    def get_parameters(self):
+        return [self.a0, self.a1, self.a2, self.a3, self.a4, self.a5]
+
+class Interpolator: 
+    """ 
+    Interpolator class to generate a trajectory between two points in a given time duration.
+    The path function is a function of time s(t) that returns 
+        the desired point at time t.
+    """
+    def __init__(self, path_function, duration):
+        self.path_function = path_function
+        self.duration = duration
+
+    def discretize_trajectory(self, rate, start_time):
+        sampling_time = 1.0 / rate  # 0.002 in case of 500 Hz
+        time = start_time  # Usually 0 at the beginning
+        points = []
+
+        # Until you reach the time taken by the publishing node to send the desired pose
+        while time < self.duration:
+            points.append(self.path_function(time))
+            time += sampling_time
+
+        remaining_time = max(0.0, self.duration - time)
+        return points, remaining_time
+
 class Controller:
-    def __init__(self, nh, control_rate):
+    def __init__(self, nh, control_rate, time_law_type="linear"):
         self.nh = nh
         self.control_rate = control_rate
-        self.kp = np.array([1, 1, 1])
-        self.ko = np.array([1, 1, 1])
+        self.time_law_type = time_law_type
+        self.kp = np.array([0.1, 0.1, 0.1])
+        self.ko = np.array([0.1, 0.1, 0.1])
         self.joint_state_sub = rospy.Subscriber("/joint_states", JointState, self.joint_state_callback)
         self.desired_pose_sub = rospy.Subscriber("/desired_pose", PoseStamped, self.desired_pose_callback)
         self.joint_velocity_pub = rospy.Publisher("/joint_group_vel_controller/command", Float64MultiArray, queue_size=1)
@@ -143,7 +227,9 @@ class Controller:
         self.buffered_orientation = []
         robot_description = rospy.get_param("robot_description", "")
         self.kinematics = Kinematics(robot_description)
-        self.kinematics.set_chain("teleop_link", "tool0")
+        self.kinematics.set_chain("base_link", "tool0")
+        self.prev_velocity = np.zeros(3)
+        self.prev_acceleration = np.zeros(3)
 
     def joint_state_callback(self, msg):
         joint_names = ["shoulder_pan_joint", "shoulder_lift_joint", "elbow_joint", "wrist_1_joint", "wrist_2_joint", "wrist_3_joint"]
@@ -159,64 +245,76 @@ class Controller:
             self.prev_position = np.array([msg.pose.position.x, msg.pose.position.y, msg.pose.position.z])
             q = [msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w]
             self.prev_orientation = R.from_quat(q).as_matrix()
+            self.prev_velocity = np.zeros(3)
+            self.prev_acceleration = np.zeros(3)
         else:
-            delta_time = 1.0 / 100.0 # put here teh frequency of the node that publsihes the desired pose
+            delta_time = 1.0 / 100.0
             current_position = np.array([msg.pose.position.x, msg.pose.position.y, msg.pose.position.z])
             q = [msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w]
             current_orientation = R.from_quat(q).as_matrix()
-            self.create_interpolators(self.prev_position, current_position, self.prev_orientation, current_orientation, delta_time)
+
+            current_velocity = (current_position - self.prev_position) / delta_time
+            current_acceleration = (current_velocity - self.prev_velocity) / delta_time
+
+            self.create_interpolators(
+                self.prev_position, current_position, 
+                self.prev_orientation, current_orientation, 
+                delta_time,
+                start_velocity=self.prev_velocity, end_velocity=current_velocity,
+                start_acceleration=self.prev_acceleration, end_acceleration=current_acceleration
+            )
+
             self.prev_position = current_position
             self.prev_orientation = current_orientation
+            self.prev_velocity = current_velocity
+            self.prev_acceleration = current_acceleration
 
+    def create_time_law(self, start, end, duration, start_velocity=None, end_velocity=None, start_acceleration=None, end_acceleration=None):
+        """
+        Factory method to create the desired time law based on the user's choice.
+        Handles both scalar and array inputs for velocity and acceleration.
+        """
+        start_velocity = start_velocity if start_velocity is not None else np.zeros_like(start)
+        end_velocity = end_velocity if end_velocity is not None else np.zeros_like(end)
+        start_acceleration = start_acceleration if start_acceleration is not None else np.zeros_like(start)
+        end_acceleration = end_acceleration if end_acceleration is not None else np.zeros_like(end)
 
-    def create_interpolators(self, start_position, end_position, start_orientation, end_orientation, duration):
-        self.interpolator_position = Interpolator(
-            lambda t: LinearTimeLaw(start_position, end_position, duration).evaluate(t), 
-            duration
-        )
-
-        ''' 
-        Compute the axis angle representation of the rotation matrix.
-        start_orientation = Rstart ==> 3x3 rotation matrix
-        end_orientation = Rend ==> 3x3 rotation matrix
-        R^i_f = deltaR = Rstart^T * Rend
-        NOTE: the code handles the case where the rotation is zero, that could lead to numerical
-            instability when computing the norm of the axis_angle vector (you should divide by zero).
-        '''
-        delta_R = np.dot(start_orientation.T, end_orientation)
-
-        # Pass to axis-angle representation (delta_R => r_vector = axis_angle)
-        # np.linalg.norm = ||axis_angle|| = rotation angle
-        axis_angle = R.from_matrix(delta_R).as_rotvec()
-      
-        # Handle zero rotation case
-        if np.linalg.norm(axis_angle) < 1e-1:  # Threshold to check for near-zero rotation
-            self.interpolator_orientation = Interpolator(
-                lambda t: start_orientation,  # No rotation; keep the start orientation
-                duration
-            )
+        if self.time_law_type == "linear":
+            return LinearTimeLaw(start, end, duration)
+        elif self.time_law_type == "cubic":
+            return CubicalTimeLaw(start, end, start_velocity, end_velocity, duration)
+        elif self.time_law_type == "quintic":
+            return QuinticTimeLaw(start, end, start_velocity, end_velocity, start_acceleration, end_acceleration, duration)
         else:
-            # LinearTimeLaw(0, np.linalg.norm(axis_angle), duration).evaluate => s(t_k)
-            # LinearTimeLaw(0, np.linalg.norm(axis_angle), duration).evaluate(t) * axis_angle = s(t_k) * r_vector
-            # R.from_rotvec(s(t_k) * r_vector) = matrix exponential exp(s(t_k) * r_vector)
-            # R_prev * exp(s(t_k) * r_vector) = R_desired (implicit in the interpolator)
+            raise ValueError(f"Unsupported time law type: {self.time_law_type}")
+
+
+    def create_interpolators(self, start_position, end_position, start_orientation, end_orientation, duration, 
+                             start_velocity=None, end_velocity=None, start_acceleration=None, end_acceleration=None):
+        time_law_position = self.create_time_law(start_position, end_position, duration, 
+                                                 start_velocity=start_velocity, end_velocity=end_velocity, 
+                                                 start_acceleration=start_acceleration, end_acceleration=end_acceleration)
+        self.interpolator_position = Interpolator(lambda t: time_law_position.evaluate(t), duration)
+
+        delta_R = np.dot(start_orientation.T, end_orientation)
+        axis_angle = R.from_matrix(delta_R).as_rotvec()
+
+        if np.linalg.norm(axis_angle) < 1e-1:
+            self.interpolator_orientation = Interpolator(lambda t: start_orientation, duration)
+        else:
+            time_law_orientation = self.create_time_law(0, np.linalg.norm(axis_angle), duration)
             self.interpolator_orientation = Interpolator(
-                lambda t: R.from_rotvec(
-                    LinearTimeLaw(0, np.linalg.norm(axis_angle), duration).evaluate(t) * 
-                    axis_angle / np.linalg.norm(axis_angle)
-                ).as_matrix(),
+                lambda t: R.from_rotvec(time_law_orientation.evaluate(t) * axis_angle / np.linalg.norm(axis_angle)).as_matrix(),
                 duration
             )
 
         self.buffered_position, _ = self.interpolator_position.discretize_trajectory(self.control_rate, 0.0)
         self.buffered_orientation, _ = self.interpolator_orientation.discretize_trajectory(self.control_rate, 0.0)
 
-
     def compute_control_action(self):
         if not self.buffered_position or not self.buffered_orientation:
             return
 
-        # Compute the 'feedback' cartesian value
         tcp_pose = self.kinematics.compute_fk(self.joint_positions)
         if tcp_pose is None:
             return
@@ -228,26 +326,22 @@ class Controller:
             [tcp_pose.M[2, 0], tcp_pose.M[2, 1], tcp_pose.M[2, 2]]
         ])
 
-        # Thanks to the interpolator, we can now get the desired position and orientation
         desired_position = self.buffered_position.pop(0)
         desired_orientation = self.buffered_orientation.pop(0)
 
-        # Calculate the errors (watch out for the orientation error...)
         position_error = desired_position - tcp_position
         orientation_error = compute_orientation_error(tcp_orientation, desired_orientation)
 
-        # Calculate the control input
         control_input_p = self.kp * position_error
         control_input_o = self.ko * orientation_error
         control_input = np.hstack((control_input_p, control_input_o))
 
-        # Pass from cartesian to joint space
         jacobian = self.kinematics.compute_jacobian(self.joint_positions)
         if jacobian is None or np.linalg.det(jacobian) == 0:
             rospy.logerr("Jacobian is singular or invalid.")
             return
         joint_velocities = np.dot(np.linalg.pinv(jacobian), control_input)
-        self.publish_joint_velocities(joint_velocities) # publish at 500 Hz
+        self.publish_joint_velocities(joint_velocities)
 
     def publish_joint_velocities(self, joint_velocities):
         msg = Float64MultiArray()
@@ -257,9 +351,17 @@ class Controller:
 if __name__ == "__main__":
     rospy.init_node("position_controller")
     control_rate = 500.0
-    rate = rospy.Rate(control_rate)
-    controller = Controller(rospy, control_rate)
+    time_law_type = "cubic"  # Options: "linear", "cubic", "quintic"
+    controller = Controller(rospy, control_rate, time_law_type)
 
+    rate = rospy.Rate(control_rate)
+
+    last_time = time.time()  # Initialize the timer
     while not rospy.is_shutdown():
+        current_time = time.time()
+        elapsed_time = current_time - last_time
+        frequency = 1.0 / elapsed_time if elapsed_time > 0 else 0  # Compute frequency
+        rospy.loginfo(f"Control Loop Frequency: {frequency:.2f} Hz")
+        last_time = current_time
         controller.compute_control_action()
         rate.sleep()
--- src/controller_main.cpp
+++ src/controller_main.cpp
@@ -1,27 +1,49 @@
 #include "../include/controller.hpp"
+#include <chrono>
+#include <vector>
+#include <numeric>
 
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "position_controller");
-	ROS_INFO("Node initialized.");
-	ros::NodeHandle nh;
+    ros::init(argc, argv, "position_controller");
+    ROS_INFO("Node initialized.");
+    ros::NodeHandle nh;
 
-	// Create the controller specific to your robot
-	std::shared_ptr<Controller> p_controller = std::make_shared<Controller>(nh, 500.0);
-	ROS_INFO("Controller object created.");
-	ros::Rate r(500);
+    // Create the controller specific to your robot
+    std::shared_ptr<Controller> p_controller = std::make_shared<Controller>(nh, 500.0);
+    ROS_INFO("Controller object created.");
 
-	while(ros::ok())
-	{
-		//auto start = std::chrono::high_resolution_clock::now();
-		p_controller->ComputeControlAction();
-		ROS_INFO("Control action computed.");
-		//auto end = std::chrono::high_resolution_clock::now();
-		//double duration_control_action = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() / 1000.0;
+    ros::Rate r(500); // Target loop rate
+    auto last_time = std::chrono::high_resolution_clock::now();
+    auto start_time = std::chrono::high_resolution_clock::now(); // For periodic logging
+    std::vector<double> frequencies; // Store frequencies for averaging
 
-		ros::spinOnce();
-		r.sleep();
-	}
-	ROS_INFO("Node terminated.");
-	return 0;
+    while (ros::ok())
+    {
+        auto current_time = std::chrono::high_resolution_clock::now();
+        double loop_time = std::chrono::duration<double>(current_time - last_time).count(); // Loop time in seconds
+        last_time = current_time;
+
+        double frequency = 1.0 / loop_time; // Compute frequency in Hz
+        frequencies.push_back(frequency);
+
+        // Periodically log the average frequency (every 10 seconds)
+        if (std::chrono::duration<double>(current_time - start_time).count() >= 10.0)
+        {
+            double avg_frequency = std::accumulate(frequencies.begin(), frequencies.end(), 0.0) / frequencies.size();
+            ROS_INFO("Average Control Loop Frequency: %.2f Hz", avg_frequency);
+            frequencies.clear(); // Reset the vector
+            start_time = current_time; // Reset the periodic timer
+        }
+
+        // Compute control action
+        p_controller->ComputeControlAction();
+        ROS_INFO("Control action computed.");
+
+        ros::spinOnce();
+        r.sleep();
+    }
+
+    ROS_INFO("Node terminated.");
+    return 0;
 }
